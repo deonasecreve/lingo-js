8aa97d6b6621947927affe2c29048d7c1043b792
DESKTOP-JBDTEOK\Deona <deona.secreve@outlook.com>
2017-02-14 14:51:36 +0100

    reset function

--- lingo.js
+++ lingo.js
@@ -1,29 +1,47 @@
-
-function getRandomwords(){
-	var randomword = words[Math.floor(Math.random()*words.length)];
-    console.log(randomword);
-    return randomword;
-}
-
 var word = getRandomwords();
 var word_split = word.split("");
 var count = 1;
 
+function getRandomwords() {
+    var randomword = words[Math.floor(Math.random() * words.length)];
+    console.log(randomword);
+    return randomword;
+}
 
-function wordCheck(){
-   var wordInput = document.getElementById("wordInput").value;
-   var letterInput = wordInput.split("");
-    for (i = 0; i < 5; i++){
-        if(letterInput[i] === wordInput[i]){
-            console.log(letterInput[i] + "=" + wordInput[i]);
+function wordCheck() {
+    if (count == 6 ) {
+        alert("stop, de beurten zijn op");
+        resetLingo();
+        getRandomwords();
+        return;
+    }
+    var wordInput = document.getElementById("wordInput").value;
+    var letterInput = wordInput.split("");
+    for (i = 0; i < 5; i++) {
+        console.log(letterInput[i] + "=" + wordInput[i]);
+        placeInputLetters(i, letterInput[i]);
+    if (word_split[i] === letterInput[i]){
+        document.getElementById("")
         }
     }
+    count++
 }
-wordCheck();
 
-function Loading(){
+function firstLetterLoading() {
     var firstLetter = document.getElementsByClassName("row1")[0].children;
     console.log(word_split[0]);
-firstLetter[0].innerHTML = "<div class='first letter'>" + word_split[0] + "</div";
+    firstLetter[0].innerHTML = "<div class='first letter'>" + word_split[0] + "</div";
 }
-Loading()
+firstLetterLoading();
+
+function placeInputLetters(pos, letter) {
+    var firstLetter = document.getElementsByClassName("row" + count)[0].children;
+    firstLetter[pos].innerHTML = "<div class='first letter'>" + letter + "</div";
+}
+
+function resetLingo(){
+    for (i = 1; i < 26; i++){
+        document.getElementById(i).innerHTML = '';
+    }
+}
+
